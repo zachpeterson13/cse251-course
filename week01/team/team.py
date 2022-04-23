1ba0c0b6a6c1be2b183eb4414f32941d7907c54d
Zach Peterson <zachpeterson13@icloud.com>
2022-04-23 14:14:00 -0600

    week01 team activity

--- week01/team/team.py
+++ week01/team/team.py
@@ -20,9 +20,13 @@ import threading
 # Include cse 251 common Python files
 from cse251 import *
 
+# Constants
+NUM_OF_THREADS = 10
+
 # Global variable for counting the number of primes found
 prime_count = 0
 numbers_processed = 0
+
 
 def is_prime(n: int) -> bool:
     global numbers_processed
@@ -36,32 +40,41 @@ def is_prime(n: int) -> bool:
     if n % 2 == 0 or n % 3 == 0:
         return False
     i = 5
-    while i ** 2 <= n:
+    while i**2 <= n:
         if n % i == 0 or n % (i + 2) == 0:
             return False
         i += 6
     return True
 
 
-if __name__ == '__main__':
+def t_func(start, end, index):
+    global prime_count
+
+    for i in range(start + index, start + range_count, NUM_OF_THREADS):
+        if is_prime(i):
+            prime_count += 1
+    #         print(i, end=", ", flush=True)
+    # print(flush=True)
+
+
+if __name__ == "__main__":
     log = Log(show_terminal=True)
     log.start_timer()
 
-    # TODO 1) Get this program running
-    # TODO 2) move the following for loop into 1 thread
-    # TODO 3) change the program to divide the for loop into 10 threads
-
     start = 10000000000
     range_count = 100000
-    for i in range(start, start + range_count):
-        if is_prime(i):
-            prime_count += 1
-            print(i, end=', ', flush=True)
-    print(flush=True)
+
+    threads = []
+
+    for i in range(NUM_OF_THREADS):
+        t = threading.Thread(target=t_func, args=(start, range_count, i))
+        threads.append(t)
+        t.start()
+
+    for t in threads:
+        t.join()
 
     # Should find 4306 primes
-    log.write(f'Numbers processed = {numbers_processed}')
-    log.write(f'Primes found      = {prime_count}')
-    log.stop_timer('Total time')
-
-
+    log.write(f"Numbers processed = {numbers_processed}")
+    log.write(f"Primes found      = {prime_count}")
+    log.stop_timer("Total time")
